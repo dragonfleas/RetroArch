f1b2e2a99ba34ce9f920ea4058edfd47b74818f9
Antonio Giner <calamity15khz@gmail.com>
2024-04-03 21:37:05 +0200

    * mister.c - Ignore bogus frames (size < 64 pixels). - Resize viewport for hardware and software renderers. - Resize viewport to actual framebuffer width/height rather than modeline hactive/vactive, to respect borders when they exist. Prevents buffer overflows.
    * video_crt_switch.c
    - Don't use force_full in set_viewport.
    - Force a default mode 640x480 for bogus resolution requests (< 64 pixels).
    
    * video_driver.c
    - Move Switchres block before frame().
    - Move MiSTer block after frame().

--- gfx/video_driver.c
+++ gfx/video_driver.c
@@ -5068,10 +5068,8 @@ void video_driver_frame(const void *data, unsigned width,
    {
       video_info.current_subframe = 0;
 
-#ifdef HAVE_MISTER /* psakhis */
-      if (config_get_ptr()->bools.video_mister_enable)
-         mister_draw(video_st, data, width, height, pitch);
-#endif
+      video_info.width = width;
+      video_info.height = height;
 
       if (vid->frame(
                video_st->data, data, width, height,
@@ -5091,6 +5089,11 @@ void video_driver_frame(const void *data, unsigned width,
    }
 
    video_st->frame_count++;
+
+#ifdef HAVE_MISTER //psakhis
+      if (config_get_ptr()->bools.video_mister_enable)
+         mister_draw(video_st, data, width, height, pitch);
+#endif
 
    /* Display the status text, with a higher priority. */
    if (  (   video_info.fps_show
