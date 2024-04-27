c3c0ee70e20103bffc78d5e142b89c35dfdf562b
Antonio Giner <calamity15khz@gmail.com>
2024-04-27 19:23:59 +0200

    - Add adaptative lz4 [psakhis] - Set lz4 compression mode through option mister_lz4:   "0" - raw   "1" - lz4   "2" - lz4hc   "3" - lz4/lz4hc adaptative

--- configuration.h
+++ configuration.h
@@ -254,6 +254,7 @@ typedef struct settings
       unsigned video_window_opacity;
       unsigned crt_switch_resolution;
       unsigned crt_switch_resolution_super;
+      unsigned mister_lz4;
       unsigned screen_brightness;
       unsigned video_monitor_index;
       unsigned video_fullscreen_x;
@@ -967,7 +968,6 @@ typedef struct settings
 
       bool crt_switch_custom_refresh_enable;
       bool crt_switch_hires_menu;
-      bool mister_lz4; //psakhis
       bool mister_scanlines;
       bool mister_force_rgb565;
       bool mister_interlaced_fb;
